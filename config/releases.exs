aada75fa2b02b97dd360141300ad4949b40e55c6
Joe Kurleto <joedoku@gmail.com>
2020-04-07 18:16:57 -0400

    Update releases config to use new host

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,6 @@ import Config
 config :gamenight, GamenightWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: "gamenight.lol", port: 443],
+  check_origin: ["//gamenight.lol", "//*.gamenight.lol"],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
